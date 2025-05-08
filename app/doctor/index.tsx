0a0b8281ff95243280be2ffc2ef811563e1299eb
franzbrnrdn <109334246+franzbrnrdn@users.noreply.github.com>
2025-05-08 09:05:41 +0800

    conflict branch fixed

--- app/doctor/index.tsx
+++ app/doctor/index.tsx
@@ -1,20 +1,10 @@
-import {
-  // ImageBackground,
-  // type ImageSourcePropType,
-  ScrollView,
-} from 'react-native';
-// import { bmsBg } from '~/assets';
+import { ScrollView } from 'react-native';
 import { LandingPage } from '~/components/doctor/landing';
 
 export default function DoctorScreen() {
   return (
-    // <ImageBackground
-    //   className="justify-center flex-1 bg-center bg-no-repeat bg-cover"
-    //   source={bmsBg as ImageSourcePropType}
-    // >
-    <ScrollView className="flex-1 bg-transparent">
+    <ScrollView className="flex-1 bg-primary-foreground">
       <LandingPage />
     </ScrollView>
-    // </ImageBackground>
   );
 }
